570cf1f0359d6ab0a8eb4b787585095084c71070
jonno2012 <jonathanrhill78@gmail.com>
2023-05-20 18:13:01 +0100

    Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors: {
+      'sky-600': '#FFD700',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
